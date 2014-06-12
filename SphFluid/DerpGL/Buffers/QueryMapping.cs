8c367c04b8af8cd25ad33aaa0b08e953069f7113
JcBernack <jc.bernack@gmail.com>
2014-06-12 04:58:30 +0200

    added wrapper for query objects
    svn-revision: 170

---
+++ SphFluid/DerpGL/Buffers/QueryMapping.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenTK.Graphics.OpenGL;
+
+namespace DerpGL.Buffers
+{
+    public class QueryMapping<T>
+        : QueryIndexer
+        where T : struct, IConvertible
+    {
+        public class QueryMap
+        {
+            internal readonly int Handle;
+
+            internal bool Active;
+            internal QueryTarget Target;
+            internal int Index;
+
+            public int Value;
+            public int Average;
+
+            public QueryMap(int handle)
+            {
+                Handle = handle;
+            }
+        }
+
+        public QueryMap this[T key]
+        {
+            get
+            {
+                return _queries[key];
+            }
+        }
+
+        private readonly Dictionary<T, QueryMap> _queries;
+        private const float AveragingFactor = 0.05f;
+
+        public QueryMapping()
+        {
+            if (!typeof(T).IsEnum) throw new ArgumentException("T must be an enumerated type");
+            // create a query object for each enum entry
+            _queries = Enum.GetValues(typeof (T)).Cast<T>().ToDictionary(_ => _, _ => new QueryMap(GL.GenQuery()));
+        }
+
+        public void Begin(T mapping, QueryTarget target)
+        {
+            var map = _queries[mapping];
+            if (map.Active) throw new ApplicationException(string.Format("Query already active: {0} {1}", target, mapping));
+            map.Active = true;
+            map.Target = target;
+            map.Index = AcquireIndex(target);
+            GL.BeginQueryIndexed(target, map.Index, map.Handle);
+        }
+
+        public void End(T mapping)
+        {
+            var map = _queries[mapping];
+            GL.EndQueryIndexed(map.Target, map.Index);
+            ReleaseIndex(map.Target, map.Index);
+            map.Active = false;
+        }
+
+        public void Update()
+        {
+            foreach (var map in _queries.Values)
+            {
+                // get current value
+                GL.GetQueryObject(map.Handle, GetQueryObjectParam.QueryResult, out map.Value);
+                // calculate averaged value
+                map.Average = (int)(map.Value * AveragingFactor + map.Average * (1 - AveragingFactor));
+            }
+        }
+    }
+}
