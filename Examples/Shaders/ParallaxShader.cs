7c83bfb2d640e7c57698d1b0810cccfbd68abdec
JcBernack <jc.bernack@gmail.com>
2014-10-03 09:34:20 +0200

    Add advanced example for parallax mapping
    Refactor OpenTK example for parallax mapping to show off some DerpGL usage

---
+++ Examples/Shaders/ParallaxShader.cs
@@ -0,0 +1,36 @@
+﻿using DerpGL.Shaders;
+using DerpGL.Shaders.Variables;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace Examples.Shaders
+{
+    [VertexShaderSource("Parallax.vs")]
+    [FragmentShaderSource("Parallax.fs")]
+    public class ParallaxShader
+        : Shader
+    {
+        [VertexAttrib(3, VertexAttribPointerType.Float)]
+        public VertexAttrib InPosition { get; protected set; }
+        [VertexAttrib(3, VertexAttribPointerType.Float)]
+        public VertexAttrib InNormal { get; protected set; }
+        [VertexAttrib(3, VertexAttribPointerType.Float)]
+        public VertexAttrib InTangent { get; protected set; }
+        [VertexAttrib(2, VertexAttribPointerType.Float)]
+        public VertexAttrib InTexCoord { get; protected set; }
+
+        public Uniform<Matrix4> ModelViewMatrix { get; protected set; }
+        public Uniform<Matrix4> ModelViewProjectionMatrix { get; protected set; }
+        public Uniform<Matrix3> NormalMatrix { get; protected set; }
+
+        public Uniform<Vector3> Light_Position { get; protected set; }
+        public Uniform<Vector3> Camera_Position { get; protected set; }
+
+        public TextureUniform Material_DiffuseAndHeight { get; protected set; }
+        public TextureUniform Material_NormalAndGloss { get; protected set; }
+        public Uniform<Vector3> Material_ScaleBiasShininess { get; protected set; }
+
+        public Uniform<Vector3> Light_DiffuseColor { get; protected set; }
+        public Uniform<Vector3> Light_SpecularColor { get; protected set; }
+    }
+}
