d519deb776b64ef70c57584ea9d8b21a3574e9c9
JcBernack <jc.bernack@gmail.com>
2014-10-06 20:25:34 +0200

    Add license header to source files

--- DerpGL/Properties/AssemblyInfo.cs
+++ DerpGL/Properties/AssemblyInfo.cs
@@ -1,4 +1,21 @@
-﻿using System.Reflection;
+﻿#region License
+// DerpGL License
+// Copyright (C) 2013-2014 J.C.Bernack
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#endregion
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -9,7 +26,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("JcBernack")]
 [assembly: AssemblyProduct("DerpGL")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2013-2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
