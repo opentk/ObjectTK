e27ffc7ba61800f1d01878e256477bff04abf873
JcBernack <jc.bernack@gmail.com>
2014-12-10 21:18:31 +0100

    Add DerpGLC to integrate shader compilation into MSBuild and Visual Studio

---
+++ DerpGLC/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DerpGLC")]
+[assembly: AssemblyDescription("Shader compiler for integrated GLSL shader validation in MSBuild and Visual Studio")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("JcBernack")]
+[assembly: AssemblyProduct("DerpGLC")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8bac61da-d508-4a19-a865-8df2c3bbda59")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.*")]
