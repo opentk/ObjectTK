3bb6f5623407f35f43039a2b62f2f4a52441de0a
burnss9 <burnss9@nku.edu>
2020-10-21 08:56:15 -0400

    how were these spaces

--- ObjectTK.Data/Variables/VertexAttributeInfo.cs
+++ ObjectTK.Data/Variables/VertexAttributeInfo.cs
@@ -2,23 +2,23 @@
 
 namespace ObjectTK.Data.Variables {
 	public class VertexAttributeInfo {
-        public int ProgramHandle { get; }
-        public string Name { get; set; }
-        public bool Active { get; set; }
-        public int Index { get; private set; }
-        public int Components { get; private set; }
-        public ActiveAttribType Type { get; private set; }
-        public bool Normalized { get; private set; }
+		public int ProgramHandle { get; }
+		public string Name { get; set; }
+		public bool Active { get; set; }
+		public int Index { get; private set; }
+		public int Components { get; private set; }
+		public ActiveAttribType Type { get; private set; }
+		public bool Normalized { get; private set; }
 
-        public VertexAttributeInfo(int ProgramHandle, string Name, bool Active, int Index, int Components, ActiveAttribType Type, bool Normalized) {
-            this.ProgramHandle = ProgramHandle;
-            this.Name = Name;
-            this.Active = Active;
-            this.Index = Index;
-            this.Components = Components;
-            this.Type = Type;
-            this.Normalized = Normalized;
+		public VertexAttributeInfo(int ProgramHandle, string Name, bool Active, int Index, int Components, ActiveAttribType Type, bool Normalized) {
+			this.ProgramHandle = ProgramHandle;
+			this.Name = Name;
+			this.Active = Active;
+			this.Index = Index;
+			this.Components = Components;
+			this.Type = Type;
+			this.Normalized = Normalized;
 		}
-    }
+	}
 
 }
